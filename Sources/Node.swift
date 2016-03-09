2a3087188229668be7ef50a46bfe00ab52c80d3d
Ivan Arellano <ia@ivanarellano.com>
2016-03-09 14:25:04 -0500

    Add custom accessors

--- Sources/Node.swift
+++ Sources/Node.swift
@@ -1,17 +1,43 @@
 class NodeBacking<Element> {
-    var backing: Element
+    
+    var data: Element
 
-    init(backing: Element) {
-        self.backing = backing
+    init(data: Element) {
+        self.data = data
     }
 }
 
 struct Node<Element> {
-    var data: NodeBacking<Element>
-    var next: NodeBacking<Node<Element>?>
+    
+    private var dataBacking: NodeBacking<Element>
+    private var nextBacking: NodeBacking<Node<Element>?>
 
     init(data: Element) {
-        self.data = NodeBacking(backing: data)
-        next = NodeBacking(backing: nil)
+        dataBacking = NodeBacking(data: data)
+        nextBacking = NodeBacking(data: nil)
+    }
+    
+    var data: Element {
+        /* Use this with Mac OS
+        mutating get {
+            if !isUniquelyReferencedNonObjC(&dataBacking) {
+                dataBacking = NodeBacking(data: dataBacking.data)
+            }
+             */
+        get {
+            return dataBacking.data
+        }
+        set {
+            dataBacking.data = data
+        }
+    }
+    
+    var next: Node<Element>? {
+        get {
+            return nextBacking.data
+        }
+        set {
+            nextBacking = NodeBacking(data: newValue)
+        }
     }
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,7 @@
-let test = EightPuzzle.manhattan(0, y1: 1, x2: 5, y2: 3)
-print(test)
+//let test = EightPuzzle.manhattan(0, y1: 1, x2: 5, y2: 3)
+//print(test)
+
+var node1 = Node(data: 11)
+print(node1.data, node1.next)
+node1.next = Node(data: 13231)
+print(node1.next?.data, node1.next?.next)
