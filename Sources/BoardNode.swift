07ed49c56e27b1d772e86c32ea09513c1b1bf9ed
Ivan Arellano <ia@ivanarellano.com>
2016-03-11 13:53:08 -0500

    Added (potentially) working placed and misplaced func

--- Sources/BoardNode.swift
+++ Sources/BoardNode.swift
@@ -1,15 +1,16 @@
 struct BoardNode {
 
     private static let goal = "1234567890"
-    private var h: Int
     private static let blankTile: Character = "0"
     private static let squareGridSize = 3
 
     private let prevBoard: Node<BoardNode>?
     private let boardLayout: String
     private let direction: Direction?
-    private let f, g: Int
-    private let blankTilePos: (Int, Int)
+    private var f: Int?
+    private let g: Int
+    private let h: Int?
+    // private let blankTilePos: (Int, Int)?
 
     private var inOpen = false
     private var possibleMoves: [Node<BoardNode>] = []
@@ -32,9 +33,9 @@ struct BoardNode {
         self.boardLayout = boardLayout
         self.direction = direction
         self.g = prevBoard != nil ? prevBoard!.data.g + 1 : g
-        self.h = hValue()
-        self.f = self.g + self.h
-        self.blankTilePos = findTile(BoardNode.blankTile)
+        //self.f = self.g + self.h
+        self.h = 1
+        //self.blankTilePos = findTile(BoardNode.blankTile)
     }
 
     private mutating func pushPossibleMove(node: Node<BoardNode>) {
@@ -42,11 +43,28 @@ struct BoardNode {
     }
 
     private func placed() -> Int {
-        return 1
+        var misplaced = false
+        var placed = 0
+        var index = 0
+
+        if boardLayout.characters.count == BoardNode.goal.characters.count {
+            while misplaced == false && index < BoardNode.squareGridSize * BoardNode.squareGridSize {
+                for i in boardLayout.characters.indices {
+                    if boardLayout[i] != BoardNode.goal[BoardNode.goal.startIndex.advancedBy(index)] {
+                        misplaced = true
+                    } else {
+                        placed += 1
+                    }
+                    index += 1
+                }
+            }
+        }
+
+        return placed
     }
 
     private func misplaced() -> Int {
-        return 1
+        return 9 - placed()
     }
 
     private func hValue() -> Int {
@@ -54,10 +72,13 @@ struct BoardNode {
     }
 
     private func findTile(tile: Character) -> (dividedArrayPos: Int, modulusArrayPos: Int) {
-        //let index = boardLayout.characters.indexOf(tile)
-        
-        let fromStart = boardLayout.startIndex
-        let index = fromStart.advancedBy(boardLayout.characters.count)
+        var index = 0
+        for i in boardLayout.characters.indices {
+            if boardLayout[i] == tile {
+                break
+            }
+            index += 1
+        }
 
         return (index / BoardNode.squareGridSize, index % BoardNode.squareGridSize)
     }
